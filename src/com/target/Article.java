a83494698ca5efaed6ba15790497878d63d43863
Katherine Gott <katherinegott@Katherines-MacBook-Pro.local>
2021-05-07 15:33:26 -0500

    created a finite state machine

--- src/com/target/Article.java
+++ src/com/target/Article.java
@@ -3,19 +3,30 @@ package com.target;
 public class Article {
     private Warranty moneyBackGuarantee;
     private Warranty expressWarranty;
+    private Warranty effectiveExpressWarranty = Warranty.VOID;
 
     public Article(Warranty moneyBackGuarantee, Warranty expressWarranty) {
+        this(moneyBackGuarantee, expressWarranty, Warranty.VOID);
+    }
+    private Article(Warranty moneyBackGuarantee, Warranty expressWarranty, Warranty effectiveExpressWarranty) {
         if (moneyBackGuarantee == null) throw new IllegalArgumentException();
         if (expressWarranty == null) throw new IllegalArgumentException();
 
         this.moneyBackGuarantee = moneyBackGuarantee;
         this.expressWarranty = expressWarranty;
+        this.effectiveExpressWarranty = effectiveExpressWarranty;
     }
 
     public Warranty getExpressWarranty() {
-        return expressWarranty;
+        return effectiveExpressWarranty;
     }
     public Warranty getMoneyBackGuarantee() {
         return moneyBackGuarantee;
+    }
+    public Article withVisibleDamage() {
+        return new Article(Warranty.VOID, this.expressWarranty, this.effectiveExpressWarranty);
+    }
+    public Article nonOperational() {
+        return new Article(this.moneyBackGuarantee, this.expressWarranty, this.effectiveExpressWarranty);
     }
 }
